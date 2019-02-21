5da93c3553ae0085d478d5ea034607a961a44f71
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2019-02-21 12:48:09 +0530

    2048

---
+++ src/twenty48/core.clj
@@ -0,0 +1,22 @@
+(ns twenty48.core
+  (:gen-class))
+
+(defn move-grid-right
+  "Moves an entire grid to the right"
+  [grid]
+  grid)
+
+(defn move-grid-left
+  "Moves an entire grid to the left"
+  [grid]
+  grid)
+
+(defn move-grid-down
+  "Moves an entire grid down"
+  [grid]
+  grid)
+
+(defn move-grid-up
+  "Moves an entire grid up"
+  [grid]
+  grid)
---
+++ test/twenty48/core_test.clj
@@ -0,0 +1,51 @@
+(ns twenty48.core-test
+  (:require [clojure.test :refer :all]
+            [twenty48.core :refer :all]))
+
+(deftest moving-grid-right
+  (testing "rows with numbers that repeat"
+    (is (= '((0 0 0 4)
+             (0 0 2 4)
+             (0 0 0 4)
+             (0 0 0 8))
+           (move-grid-right
+            '((0 0 2 2)
+              (0 2 0 4)
+              (2 0 2 0)
+              (0 4 4 0)))))))
+
+(deftest moving-grid-left
+  (testing "rows with numbers that repeat"
+    (is (= '((4 0 0 0)
+             (2 4 0 0)
+             (4 0 0 0)
+             (8 0 0 0))
+           (move-grid-left
+            '((0 0 2 2)
+              (0 2 0 4)
+              (2 0 2 0)
+              (0 4 4 0)))))))
+
+(deftest moving-grid-up
+  (testing "rows with numbers that repeat"
+    (is (= '((2 2 4 2)
+             (0 4 4 4)
+             (0 0 0 0)
+             (0 0 0 0))
+           (move-grid-up
+            '((0 0 2 2)
+              (0 2 0 4)
+              (2 0 2 0)
+              (0 4 4 0)))))))
+
+(deftest moving-grid-down
+  (testing "rows with numbers that repeat"
+    (is (= '((0 0 0 0)
+             (0 0 0 0)
+             (0 2 4 2)
+             (2 4 4 4))
+           (move-grid-down
+            '((0 0 2 2)
+              (0 2 0 4)
+              (2 0 2 0)
+              (0 4 4 0)))))))
