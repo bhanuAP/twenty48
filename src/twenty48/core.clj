611392534ee2ee85eb52ed218b7e83b502da61ba
Bhanu Teja <bhanuteja5481@gmail.com>
2019-02-23 01:14:49 +0530

    add move left and right logic

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -20,3 +20,36 @@
   "Moves an entire grid up"
   [grid]
   grid)
+
+
+
+(def add-front-zeros (comp (partial take-last 4) (partial concat '(0 0 0 0))))
+
+(def combine-tiles (partial map (partial apply +)))
+
+(def get-tiles-to-be-merged (partial mapcat (partial partition-all 2)))
+
+(def partition-by-identity (partial partition-by identity))
+
+(def filter-zeros (partial remove zero?))
+
+(def add-trailing-zeros (comp reverse add-front-zeros))
+
+; start
+
+(def move-right (comp
+                 add-front-zeros
+                 combine-tiles
+                 get-tiles-to-be-merged
+                 partition-by-identity
+                 filter-zeros))
+
+(def move-left (comp
+                add-trailing-zeros
+                combine-tiles
+                get-tiles-to-be-merged
+                partition-by-identity
+                filter-zeros))
+
+(move-right '(0 2 0 2))
+(move-left '(0 2 0 2))
